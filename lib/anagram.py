e0b321daa409de63fd5ed5647efb25c93cb2c8d8
leonkinyanjui <dapijon@gmail.com>
2023-11-30 01:25:06 +0300

    Assignment complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+
+        for candidate in words:
+            if self.is_anagram(candidate):
+                matches.append(candidate)
+
+        return matches
+    
+
+        pass
